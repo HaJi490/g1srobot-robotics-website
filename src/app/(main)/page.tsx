09b395ea40be4299cb01e254a238a08527fc054b
Haji490 <99gkwldnjs99@gmail.com>
2026-01-30 19:11:15 +0900

    heroSection

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,28 +1,14 @@
 // 랜딩페이지 ISR로 sEO
+import HeroSection from '@/components/pages/main/HeroSection';
 import IndustrySection from '@/components/pages/main/IndustrySection';
 import ProductSection from '@/components/pages/main/ProductSection';
 import UseCaseSection from '@/components/pages/main/UseCaseSection';
 import CtaSection from '@/components/pages/main/CtaSection';
-import Footer from '@/components/elements/footer/Footer';
-import { MoveRight } from 'lucide-react';
 
 export default function Home() {
   return (
     <div className='flex flex-col gap-30'>
-      <div className="w-full h-screen bg-black 
-                    flex flex-col items-center justify-center
-                    text-white"
-      >
-        <h1 className="text-4xl">Lorem ipsum dolor sit amet do eiusmod</h1>
-        <p className="">consectetur adipiscing elit, sed do eiusmod tempor incididun</p>
-        <button className="mt-10 px-8 py-4 bg-white/30 rounded-full 
-                          flex gap-2 items-center text-sm
-                          hover:bg-white hover:text-black cursor-pointer"
-        >
-          G1sRobot과 함께 상상을 실현해 보세요
-          <MoveRight size={14} />
-        </button>
-      </div>
+      <HeroSection/>
       <IndustrySection />
       <ProductSection />
       <UseCaseSection />
--- src/components/pages/main/HeroSection.tsx
+++ src/components/pages/main/HeroSection.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import ScrollReveal from '@/motions/ScrollReveal';
+import { MoveRight } from 'lucide-react';
 
 export default function HeroSection() {
   return (
-    <div>
-      
+    <div className="w-full h-screen bg-black 
+                    flex flex-col items-center justify-center
+                    text-white"
+    >
+      <ScrollReveal>
+        <h1 className="text-4xl">Lorem ipsum dolor sit amet do eiusmod</h1>
+        <p className="">consectetur adipiscing elit, sed do eiusmod tempor incididun</p>
+        <button className="mt-10 px-8 py-4 bg-white/30 rounded-full
+                            flex gap-2 items-center text-sm
+                            hover:bg-white hover:text-black cursor-pointer"
+        >
+          G1sRobot과 함께 상상을 실현해 보세요
+          <MoveRight size={14} />
+        </button>
+      </ScrollReveal>
     </div>
   )
 }
